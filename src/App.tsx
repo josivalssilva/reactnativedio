f57b7490c37241fb9908158220df8af8f7270e3a
josivalssilva <josivalssilva@gmail.com>
2021-11-23 06:22:41 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableOpacityBase,
+  Text,
+
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+    
+    <SafeAreaView>
+      <Text>Hello Word</Text>
+      <TouchableOpacity><Text>Gerar número</Text></TouchableOpacity>
+    </SafeAreaView>
+    </>
+  );
+};
+export default App;
